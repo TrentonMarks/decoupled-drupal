819c3c1e5736b98b73e286a3713e0451c8ead667
Trenton Marks <trentonmarks@Trentons-MacBook-Air.local>
2020-10-01 10:44:57 -0400

    Reconstructed initial html structure from body to App component

---
+++ sites/default/files/php/twig/5f74cd1ee954d___string_template__bd0919_4xUkHDxnPgeQhltTK4KHXir5N/tg8gu7ZKAABmxp5tv3m75llRlNjSLY-A2EK-gj2-B-E.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__bd091953750d7e6861114797bfad7f2e6a7c1ae569eb5245127eb2b3091e6b4b */
+class __TwigTemplate_ab1d7bae476d4a7f7712a876393ff8963f62a3cbaae2a99bf89d26db81768705 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<code>/admin/structure/webform/manage/{webform}/submission/{webform_submission}</code>: GET, <del>PATCH</del>, <del>DELETE</del></br><code>/entity/webform_submission</code>: POST";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__bd091953750d7e6861114797bfad7f2e6a7c1ae569eb5245127eb2b3091e6b4b";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__bd091953750d7e6861114797bfad7f2e6a7c1ae569eb5245127eb2b3091e6b4b", "");
+    }
+}
